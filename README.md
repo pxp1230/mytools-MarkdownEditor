04573dba9abde06a5a48292ba627e3c527b4c4fa
潘霄鹏 <2423200363@qq.com>
2018-04-05 16:34:28 +0800

    .

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # mytools-MarkdownEditor
 
 极简Markdown编辑器，所有操作通过快捷键完成，不支持undo/redo
+必须放在路径： `C:\Windows\MarkdownEditor.exe` 
 
 注意：
 * 需要预先安装 [pxp1230/mytools-auto-generate-HTML-files](https://github.com/pxp1230/mytools-auto-generate-HTML-files)
--- WindowsFormsApplication1/Api/FileTypeRegister.cs
+++ WindowsFormsApplication1/Api/FileTypeRegister.cs
@@ -2,6 +2,7 @@ using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,13 +20,47 @@ public static class FileTypeRegister
         }
         if (!isOk)
         {
-            Registry.ClassesRoot.CreateSubKey(extension).SetValue("", extension.Substring(1), RegistryValueKind.String);
-            string exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
-            using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(extension.Substring(1)))
+            if (IsAdministrator())
             {
-                key.CreateSubKey(@"Shell\DefaultIcon").SetValue("", exePath + ",0", RegistryValueKind.ExpandString);
-                key.CreateSubKey(@"Shell\Open\Command").SetValue("", exePath + " \"%1\"", RegistryValueKind.ExpandString);
+                Registry.ClassesRoot.CreateSubKey(extension).SetValue("", extension.Substring(1), RegistryValueKind.String);
+                //string exePath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+                string exePath = @"C:\Windows\MarkdownEditor.exe";
+                using (RegistryKey key = Registry.ClassesRoot.CreateSubKey(extension.Substring(1)))
+                {
+                    key.CreateSubKey(@"Shell\DefaultIcon").SetValue("", exePath + ",0", RegistryValueKind.ExpandString);
+                    key.CreateSubKey(@"Shell\Open\Command").SetValue("", exePath + " \"%1\"", RegistryValueKind.ExpandString);
+                }
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("首次运行时需要以管理员身份运行，以写入注册表关联.md文件。", "首次运行提醒", System.Windows.Forms.MessageBoxButtons.OK);
             }
         }
+    }
+
+    /// <summary>
+    /// 确定当前主体是否属于具有指定 Administrator 的 Windows 用户组
+    /// </summary>
+    /// <returns>如果当前主体是指定的 Administrator 用户组的成员，则为 true；否则为 false。</returns>
+    public static bool IsAdministrator()
+    {
+        bool result;
+        try
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            result = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            //http://www.cnblogs.com/Interkey/p/RunAsAdmin.html
+            //AppDomain domain = Thread.GetDomain();
+            //domain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            //WindowsPrincipal windowsPrincipal = (WindowsPrincipal)Thread.CurrentPrincipal;
+            //result = windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        catch
+        {
+            result = false;
+        }
+        return result;
     }
 }
